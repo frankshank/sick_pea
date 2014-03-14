6bcb1fe2136ef856b33dc892d8220d73f3b2ea4d
Frank Shank <oak@Grahames-MacBook-Air.local>
2014-03-14 13:10:59 +0000

    Non funtional hashtable added

--- ch2/generic_ops.rkt
+++ ch2/generic_ops.rkt
@@ -3,47 +3,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 3 different ways to slice the table
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; example hash table for meking our dispatch table
+(define ht (make-hash))
+(define (put type op proc) (hash-set! ht (list type op) proc))
+(define (get type op) (hash-ref ht (list type op)))
+
+
+;; selectors
 (define (type-tag x)
   (car x))
   
 (define (contents x)
   (cdr x))
 
-(define (put type-tag operation procedure)
-  (displayln " ++ put procedure Not implemented yet"))
-
-(define (get type op)
-  (displayln "get not implemented yet")
-  (lambda (x) x))
-
-;; conventional style
+;; conventional style on procedure for each shape
 (define (area shape)
   (cond ((eq? (type-tag shape) 'square)
          (* (contents shape) (contents shape)))
         ((eq? (type-tag shape) 'circle)
          (* pi (contents shape) (contents shape)))
         (else (error "Unknown shape"))))
-
-;; data directed programmin
+ 
+;; data directed programming data structure know what operations they can perform
 (put 'square 'area (lambda (s) (* s s)))
 (put 'circle 'area (lambda (r) (* pi (* r r))))
 
 (get 'square 'area)
 ((get 'square 'area) 4)
 
-; generic operator procedure
+;; generic operator procedure
 (define (operate op obj)
   (let ((proc (get (type-tag obj) op)))
     (if proc
         (proc (contents op))
         (error "Unknown operator for type"))))
   
-; syntactic sugaring that wraps operate
+;; syntactic sugaring that wraps operate
 (define (area2 shape)
   (operate 'area shape))
  
 ;; message passing
-;; return the data as represented as a procedure
+;; return the data represented as a procedure
 ;; no longer useing tagged data
 (define (make-square side)
   (lambda (message)
@@ -53,6 +54,6 @@
            (* 4 side))
           (else (error "Unknown message")))))
           
-; note we have to re-define operate
+;; note we have to re-define operate
 (define (operate2 op obj)
   (obj op))
--- ch2/sec_2_4.rkt
+++ ch2/sec_2_4.rkt
@@ -1,71 +1,114 @@
 #lang racket
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Excercise 2.73
+;;
+;; The derive program for symbolic differentiation
+;; can be transformed into a data directed style by
+;; re-writing the deriv procedure.
+;; Our job is to write the procedures for sums and
+;; products and the auxilliary code required to install
+;; them into our optable dispatch table
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; hash table
+;; first up the dispatch table as a (note non-functional) hashtable
 (define +optable+ (make-hash))
 (define (put op type proc) (hash-set! +optable+ (list op type) proc))
-(define (get op type_ (hash-ref +optable+) (list op type)))
+(define (get op type) (hash-ref +optable+ (list op type)))
 
-(define (attach-tag type-tag contents)
-  (cons type-tag contents))
-`
-(define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum: TYPE-TAG" datum)))
+;; the re-written deriv procedure
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp) var))))
 
-(define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum: CONTENTS" datum)))
+;; the selectors
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+; other pish
+(define (attach-tag tag exp)
+  (cons tag exp))
+
+;; the predicates that we had before from section 2.3.2
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2) 
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+;; the sum derivative package in bonkers closure fashion
+(define (install-sum-package) 
+  (define (sum-deriv expr var)
+    (make-sum (deriv (addend expr) var) 
+              (deriv (augend expr) var)))
+  (define (addend expr) (car expr))
+  (define (augend expr) (cadr expr))
+  (define (make-sum x1 x2)
+    (cond ((and (number? x1) (number? x2)) (+ x1 x2))
+          ((eq? x1 0) x2)
+          ((eq? x2 0) x1)
+          (else (list '+ x1 x2))))
+  (define (mul-deriv expr var)
+    (make-sum (make-product (multiplier expr)
+                            (deriv (multiplicand expr) var))
+              (make-product (multiplicand expr)
+                            (deriv (multiplier expr) var))))
+  (define (multiplier expr) (car expr))
+  (define (multiplicand expr) (cadr expr))
+  (define (make-product x1 x2)
+    (cond ((and (number? x1) (number? x2)) (* x1 x2))
+          ((= x1 1) x2)
+          ((= x2 1) x2)
+          ((or (= x1 0) (= x2 0)) 0)
+          (else (list '* x1 x2))))
+  (put 'deriv '+ sum-deriv)
+  (put 'deriv '* mul-deriv)
+  'done)
+
+(define (make-sum x y) 
+   ((get 'make-sum '+) x y)) 
 
 
-(define (install-rectangular-package)
-  ;; internal procedures
-  (define (real-part z) (car z))
-  (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) (cons x y))
-  (define (magnitude z) exp)
-    (sqrt (+ (square (real-part z))
-             (square (imag-part z)))))
-  (define (angle z)
-    (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a)
-    (cons (* r (cos a)) (* r (sin a))))
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag ’rectangular x))
-    (put ’real-part ’(rectangular) real-part)  
-    (put ’imag-part ’(rectangular) imag-part)
-    (put ’magnitude ’(rectangular) magnitude)
-    (put ’angle ’(rectangular) angle)
-    (put ’make-from-real-imag ’rectangular
-      (lambda (x y) (tag (make-from-real-imag x y))))
-    (put ’make-from-mag-ang ’rectangular
-      (lambda (r a) (tag (make-from-mag-ang r a))))
-’done)
+;; the product package
+(define (install-product-package) 
+   (define (make-product m1 m2) (cons m1 m2)) 
+   (define (multiplier p) (cadr p)) 
+   (define (multiplicand p) (caddr p)) 
+   (define (deriv-product exp var) 
+     (make-sum 
+      (make-product (multiplier exp) 
+                    (deriv (multiplicand exp) var))
+      (make-product (deriv (multiplier exp) var)
+                    (multiplicand exp))))
+  
+   (define (tag x) (attach-tag '* x)) 
+   (put 'deriv '(*) deriv-product) 
+   (put 'make-product '* 
+        (lambda (x y) (tag (make-product x y)))) 
+  'done) 
+
+(define (make-product x y) 
+   ((get 'make-product '*) x y))
 
 
-; derivative sum
-(define (sum-d exp var)
-  (make-sum (deriv (addend exp) var)
-            (deriv (augend exp) var)))
+;; add the exponentiation rule
+(define (exponentiation-deriv expr var) 
+  (make-product (exponent expr) 
+                (make-product 
+                 (make-exponentiation (base expr)
+                                      (make-sum (exponent expr) -1))
+                 (deriv (base expr) var))))
+(define (exponent expr) (cadr expr))
+(define (base expr) (car expr))
+(define (make-exponentiation base exponent)
+  (cond ((= exponent 0) 1)
+        ((= exponent 1) base)
+        ((= base 1) 1)
+        (else (list '** base exponent))))
+  
+(put 'deriv '** exponentiation-deriv)  
 
-(put 'deriv 'sum sum-d)
-
-; derivative of a product
-(define (prod-d exp var)
-  (make-prod (deriv (addend exp) var)
-             (deriv (augend exp) var)))
-
-(put 'deriv 'prod prod-d)
-
-; derivative of an exponent
-(define (exp-d exp var)
-   (* (exponent exp)
-      (coeff exp)
-      (- (exponent exp) 1)))
-
-(put 'deriv 'exponent exp-d)
- 
-; real call 
-(define poly (* 3 (square x)))
-(get 'deriv 'sum poly x)
+; real example 3x^2 +4x
+(define poly '(3x**2 + 4x))
+(display "Derivative of ")
+(displayln poly)
+(display " is ")
+(displayln (deriv poly 'x))
